3a305c83d714ccbcce6aa82578361c7ce1847f51
Andrew <contact@noro.cc>
2025-05-07 02:10:56 +0200

    Update +page.svelte
    Signed-off-by: Andrew <contact@noro.cc>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,9 @@
 <h1 class="text-5xl text-center my-4">
   Welcome to
   {#if page.url.hostname === 's.ily.cat'}
-		<a href="https://s.ily.cat" class="gradient">s.ily.cat</a>
-	{:else}
-		<span class="text-black text-shadow-lg text-shadow-color-gray-5">Delta</span>
+    <a href="https://s.ily.cat" class="gradient">s.ily.cat</a>
+  {:else}
+    <span class="text-black text-shadow-lg text-shadow-color-gray-5">Delta</span>
   {/if}
 </h1>
 
@@ -38,20 +38,20 @@
 
 
 <style>
-	@keyframes animate-gradient {
-		0% {
-			background-position: 0% 50%;
-		}
-		100% {
-			background-position: 200% 50%;
-		}
-	}
-	.gradient {
-		animation: animate-gradient 3s linear infinite;
-		background: linear-gradient(90deg, #ff0000, #00ff00, #0000ff, #ffff00, #00ffff, #ff00ff, #ff0000);
-		background-size: 200% 100%;
-		color: transparent;
-		-webkit-background-clip: text;
-		background-clip: text;
-	}
+  @keyframes animate-gradient {
+    0% {
+      background-position: 0% 50%;
+    }
+    100% {
+      background-position: 200% 50%;
+    }
+  }
+  .gradient {
+    animation: animate-gradient 3s linear infinite;
+    background: linear-gradient(90deg, #ff0000, #00ff00, #0000ff, #ffff00, #00ffff, #ff00ff, #ff0000);
+    background-size: 200% 100%;
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
 </style>
