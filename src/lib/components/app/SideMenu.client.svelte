8163165efe7933adb5a9246db8da62d08799ac68
Andrew <contact@noro.cc>
2025-05-13 06:16:04 +0200

    revert: Update SideMenu.client.svelte
    Signed-off-by: Andrew <contact@noro.cc>

--- src/lib/components/app/SideMenu.client.svelte
+++ src/lib/components/app/SideMenu.client.svelte
@@ -84,13 +84,11 @@
     const deltaX = current[0] - start[0];
     const deltaY = current[1] - start[1];
 
-    //// If vertical movement is greater, ignore the move
-    if (Math.abs(deltaY) /*>*/< Math.abs(deltaX)) {
-    //  $sidemenu.style.transform = '';
-    //  return (isSwiping = false);
-    //} else {
-        event.preventDefault();
-    }
+    // If vertical movement is greater, ignore the move
+    if (Math.abs(deltaY) > Math.abs(deltaX)) {
+      $sidemenu.style.transform = '';
+      return (isSwiping = false);
+    } else event.preventDefault();
 
     let newX = firstLeft + deltaX;
 
