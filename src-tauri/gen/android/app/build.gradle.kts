8bdd749ab9874d755078f775c60823348db3f553
norowachi <contact@noro.cc>
2025-11-21 21:16:01 +0100

    fix: update tauri release workflow to include project path and adjust release signing configuration

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -52,7 +52,8 @@ android {
             }
         }
         getByName("release") {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 *fileTree(".") { include("**/*.pro") }
                     .plus(getDefaultProguardFile("proguard-android-optimize.txt"))
